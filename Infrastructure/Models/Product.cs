29e3ee3fe0e5363e757f638f290a04923004a366
Mehwish Bajwa <mvbajwa@gmail.com>
2025-10-15 23:01:33 +0200

    Model Product is Created with Properties.

---
+++ Infrastructure/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Infrastructure.Models
+{
+    public class Product
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public decimal Price { get; set; }
+    }
+}
