e7b4eb5c2a77196b3b598fc35294a64024f5554b
Mehwish Bajwa <mvbajwa@gmail.com>
2025-10-16 00:06:49 +0200

    IFileService and FileResult is created

---
+++ Infrastructure/Interface/IFileService.cs
@@ -0,0 +1,9 @@
+﻿using Infrastructure.Models;
+
+namespace Infrastructure.Interface;
+
+public interface IFileService
+{
+    FileResult SaveContentToFile(string path, string content);
+    FileResult GetContentFromFile(string path);
+}
